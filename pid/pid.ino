67ab7e4ffb8bf89f4faf3ceaa4d6480467b3d31b
polmes <pol.developer1@gmail.com>
2016-12-11 19:09:22 +0100

    Some ideas to be tested for performance

--- pid/pid.ino
+++ pid/pid.ino
@@ -20,6 +20,7 @@
 
 int percent = 0;
 double in = 0, out = 0, kp = 100; // kp pot ser molt alta per fregament i inèrcia del sistema
+bool flag = false;
 
 void setup() {
   pinMode(FAN, OUTPUT);
@@ -36,31 +37,37 @@ void setup() {
 }
 
 void loop() {
-  // Comunicació, etc.
-  if (Serial.available() > 0) {
-    // Input és % (s'haurà de comprovar si > 100, etc.)
-    percent = Serial.parseInt();
+  while (true) {
+    // Comunicació, etc.
+    if (Serial.available() > 0) {
+      // Input és % (s'haurà de comprovar si > 100, etc.)
+      percent = Serial.parseInt();
+      Serial.println(percent);
+    }
+  
+    // Serial.print(out); // Serial en interrupt no funciona
+    // Serial.print(" - ");
+    // Serial.println(in);
+    // Serial.println(percent - in);
     Serial.println(percent);
+    Serial.println(in);
   }
 
-  // Serial.print(out); // Serial en interrupt no funciona
-  // Serial.print(" - ");
-  // Serial.println(in);
-  // Serial.println(percent - in);
-  Serial.println(percent);
-  Serial.println(in);
+  if (flag) {
+    in = analogRead(POT) * 100.0/384; // mitjana per eliminar soroll
+    // Serial.println(in);
+    
+    out = kp * (percent - in) * 10.24; // kp hauria de ser f(percent)
+    if (out > 1024) out = 1024;
+    if (out < 0) out = 0;
+    // Serial.println(percent);
+    // Serial.println(out);
+    
+    Timer1.setPwmDuty(FAN, out);
+  }
 }
 
 void control() {
-  in = analogRead(POT) * 100.0/384; // mitjana per eliminar soroll
-  // Serial.println(in);
-  
-  out = kp * (percent - in) * 10.24; // kp hauria de ser f(percent)
-  if (out > 1024) out = 1024;
-  if (out < 0) out = 0;
-  // Serial.println(percent);
-  // Serial.println(out);
-  
-  Timer1.setPwmDuty(FAN, out);
+  flag = true;
 }
 
