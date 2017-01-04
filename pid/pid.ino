c7e97b37e7b8fcdde2c0183f61e75a6b23012e13
polmes <pol.developer1@gmail.com>
2017-01-04 03:02:12 +0100

    Upload final fase3b (PID)

--- pid/pid.ino
+++ pid/pid.ino
@@ -1,73 +1,84 @@
 #include <TimerOne.h>
 
-/**
- * FASE 2 - Control Proporcional
- */
-
-/**
- * Concepte: D(t) = Kp * e(t)
- * Implementació: pwm_n = Kp = (target_perc_n - perc_n)
- * En augmentar Kp arriba a ser inestable (valor raonable)
- */
-
-/**
- * Trobar Kp crítica i període d'oscil·lació
- * Circuit #4
- */
-
+// Arduino
 #define FAN 9
 #define POT 0
+#define T_SAMPLE 0.002
 
-int percent = 0;
-double in = 0, out = 0, kp = 100; // kp pot ser molt alta per fregament i inèrcia del sistema
-bool flag = false;
+// PID constants
+const float kp = 19.2;
+const float ki = 200.0;
+const float kd = 0.025;
+
+// PID variables
+int input = 0.0;
+double angle = 0.0;
+double output = 0.0;
+double e = 0.0, e_prev = 0.0, sum_e = 0.0;
+int maximum = 1, minimum = 0;
+
+void control();
 
 void setup() {
   pinMode(FAN, OUTPUT);
   pinMode(POT, INPUT);
 
-  // Calibració
-  
-  Timer1.initialize(2000);
-  Timer1.attachInterrupt(control);
-
   Serial.begin(9600);
 
-  Timer1.pwm(FAN, out);
+  // Calibration
+  Serial.print("Starting calibration... ");
+  digitalWrite(LED_BUILTIN, HIGH);
+  while (millis() < 5000) {
+    angle = analogRead(POT);
+    if (angle > maximum) maximum = angle;
+    else if (angle < minimum) minimum = angle;
+  }
+  digitalWrite(LED_BUILTIN, LOW);
+  Serial.print("done. Maximum recorded value: ");
+  Serial.println(maximum);
+
+  // Setup TimerOne
+  Timer1.initialize(2000); // interrupts every 2000us == 500Hz frequency
+  Timer1.attachInterrupt(control);
+  Timer1.pwm(FAN, output);
 }
 
 void loop() {
-  while (true) {
-    // Comunicació, etc.
-    if (Serial.available() > 0) {
-      // Input és % (s'haurà de comprovar si > 100, etc.)
-      percent = Serial.parseInt();
-      Serial.println(percent);
-    }
-  
-    // Serial.print(out); // Serial en interrupt no funciona
-    // Serial.print(" - ");
-    // Serial.println(in);
-    // Serial.println(percent - in);
-    Serial.println(percent);
-    Serial.println(in);
+  // Communication
+  if (Serial.available() > 0) {
+    input = Serial.parseInt(); // read user input from 0% to 100%
+    if (input > 100) input = 100;
+    else if (input < 0) input = 0;
+    Serial.print("New input value: ");
+    Serial.println(input);
   }
 
-  if (flag) {
-    in = analogRead(POT) * 100.0/384; // mitjana per eliminar soroll
-    // Serial.println(in);
-    
-    out = kp * (percent - in) * 10.24; // kp hauria de ser f(percent)
-    if (out > 1024) out = 1024;
-    if (out < 0) out = 0;
-    // Serial.println(percent);
-    // Serial.println(out);
-    
-    Timer1.setPwmDuty(FAN, out);
-  }
+  // Debug
+  Serial.print("Sensor: ");
+  Serial.print(angle); // %
+  Serial.print(" - ");
+  Serial.print("Output: "); // 0 to 1024
+  Serial.println(output);
 }
 
 void control() {
-  flag = true;
+  angle = analogRead(POT) * 100.0 / (maximum - minimum); // read position and convert to % of available range
+  
+  e_prev = e; // previous error
+  e = input - angle ; // current error
+  sum_e = sum_e + e; // sum of errors
+  
+  output = (kp * e + ki * T_SAMPLE * sum_e + kd * (e - e_prev) / T_SAMPLE) * 10.24;
+  
+  // Corrections
+  if (output > 1024) {
+    output = 1024;
+    sum_e = (output / 10.24 - kd * (e - e_prev) / T_SAMPLE - kp * e) / (ki * T_SAMPLE);
+  } else if (output < 0) {
+    output = 0;
+    sum_e = (output / 10.24 - kd * (e - e_prev) / T_SAMPLE - kp * e) / (ki * T_SAMPLE);
+  }
+  
+  Timer1.setPwmDuty(FAN, output); // output from 0 to 1024
 }
 
