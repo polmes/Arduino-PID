d4ae91e6201a1ab06ebeb57ac42fd2cd1045dfa3
polmes <pol.developer1@gmail.com>
2017-01-04 02:14:56 +0100

    Uploaded final fase1a

--- pid/pid.ino
+++ pid/pid.ino
@@ -1,73 +1,37 @@
-#include <TimerOne.h>
-
-/**
- * FASE 2 - Control Proporcional
- */
-
-/**
- * Concepte: D(t) = Kp * e(t)
- * Implementació: pwm_n = Kp = (target_perc_n - perc_n)
- * En augmentar Kp arriba a ser inestable (valor raonable)
- */
-
-/**
- * Trobar Kp crítica i període d'oscil·lació
- * Circuit #4
- */
-
-#define FAN 9
+// Arduino
 #define POT 0
 
-int percent = 0;
-double in = 0, out = 0, kp = 100; // kp pot ser molt alta per fregament i inèrcia del sistema
-bool flag = false;
+// Variables
+int angle = 0;
+int maximum = 1, minimum = 0;
 
 void setup() {
-  pinMode(FAN, OUTPUT);
   pinMode(POT, INPUT);
-
-  // Calibració
-  
-  Timer1.initialize(2000);
-  Timer1.attachInterrupt(control);
 
   Serial.begin(9600);
 
-  Timer1.pwm(FAN, out);
+  // Calibration
+  Serial.print("Starting calibration... ");
+  digitalWrite(LED_BUILTIN, HIGH);
+  while (millis() < 5000) {
+    angle = analogRead(POT);
+    if (angle > maximum) maximum = angle;
+    else if (angle < minimum) minimum = angle;
+  }
+  digitalWrite(LED_BUILTIN, LOW);
+  Serial.print("done. Maximum recorded value: ");
+  Serial.println(maximum);
 }
 
 void loop() {
-  while (true) {
-    // Comunicació, etc.
-    if (Serial.available() > 0) {
-      // Input és % (s'haurà de comprovar si > 100, etc.)
-      percent = Serial.parseInt();
-      Serial.println(percent);
-    }
+  angle = analogRead(POT);
   
-    // Serial.print(out); // Serial en interrupt no funciona
-    // Serial.print(" - ");
-    // Serial.println(in);
-    // Serial.println(percent - in);
-    Serial.println(percent);
-    Serial.println(in);
-  }
+  // Communication
+  Serial.print("Sensor: ");
+  Serial.print(angle);
+  Serial.print(" - ");
+  Serial.print(angle * 100.0 / (maximum - minimum));
 
-  if (flag) {
-    in = analogRead(POT) * 100.0/384; // mitjana per eliminar soroll
-    // Serial.println(in);
-    
-    out = kp * (percent - in) * 10.24; // kp hauria de ser f(percent)
-    if (out > 1024) out = 1024;
-    if (out < 0) out = 0;
-    // Serial.println(percent);
-    // Serial.println(out);
-    
-    Timer1.setPwmDuty(FAN, out);
-  }
-}
-
-void control() {
-  flag = true;
+  delay(20);
 }
 
